31ec8476f144255b2141d48a1c0c24608848409c
Slauva <v.koshman@innopolis.university>
2022-03-25 19:05:46 +0300

    Create React Project

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+// External Libraries
+import React, {FC} from 'react';
+
+const App: FC = () => {
+    return (
+        <div>
+            Hello World
+        </div>
+    );
+};
+
+export default App;
